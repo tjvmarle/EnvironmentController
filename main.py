091f0a687010ec49f0e16b4343c70bacf2793e5d
tjvanmarle <metalananas@gmail.com>
2021-09-26 11:12:11 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Goal: Create a controller to allow a bunch of garlic in an isolated space to turn black over the course of 4-8 weeks
+
+#TODO:
+# Track temperature:  2-3 probes
+#   Should keep around 60-70 C
+# Track humidity
+#   Should keep around 85%
+# Control a fan for convection
+# Control a heater - Nichrome or power resistors?
+# Optional: Some device to increase humidity
